f74461316552b6eae7c19304ccff1de2f0281fcb
PreetiPolai <preetipolai@gmail.com>
2020-10-11 17:10:28 +0530

    Added InOrderTraversal.py

---
+++ Trees/Binary Search Tree/Inorder Traversal.py
@@ -0,0 +1,14 @@
+def inOrderUsingStack(root):
+    s=deque()
+    curr=root
+    while True:
+        if curr:
+            while curr:
+                s.append(curr)
+                curr=curr.left
+        elif s:
+            curr=s.pop()
+            print(curr.data,end=" ")
+            curr=curr.right
+        else:
+            break
